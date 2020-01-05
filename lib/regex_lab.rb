7885a455cbb8eebf0d8ea8f59b936145b245738e
Mikael Kaya <kayaswork@gmail.com>
2020-01-05 22:12:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  return text.scan /[aeiou]/
+  return text.scan /[aeiou]{2}/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   return text.scan /[un|ing|]/
+   return text.scan /un|ing|/
 end
 
 def words_five_letters_long(text)
