6f2e70aa50c0b8b2791c84e358752417cb6c4d98
Mikael Kaya <kayaswork@gmail.com>
2020-01-05 22:18:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   return text.scan(/un\w+ing/)
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  return text.match()
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
